d689a23c097899455c83095d34cdb7c1611ef972
Olli Kärkkäinen <olli.karkkainen@bang.fi>
2020-09-15 12:41:02 +0300

    edit fixed footer & parallax

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Parallax } from "react-parallax";
+import AOS from 'aos';
 import background from '../images/timeline-parallax-bg.jpg';
 import parallaxFront from '../images/timeline-parallax-front.png';
 
 const History = () => {
+
+    const initTimeline = () => {
+        AOS.init()
+    }
+    useEffect(initTimeline, [])
+
     return (
         <Parallax 
             bgImage={background} 
@@ -23,21 +30,21 @@ const History = () => {
                                 <h2>Education</h2>
                             </div>
                             <ul className="history-timeline">
-                                <li>
+                                <li data-aos="fade-up" data-aos-duration="2000" data-aos-offset="100">
                                     <h4>JAMK University of Applied Sciences</h4>
                                     <p>Bachelor of Business Administration (BBA)</p>
                                     <p>Business Information Technology</p>
                                     <p>2015–2019</p>
                                     <p className="italic">My studies focused on web application and game development as well as their marketing and business.</p>
                                 </li>
-                                <li>
+                                <li data-aos="fade-up" data-aos-duration="2000" data-aos-offset="100">
                                     <h4>Universidad Argentina de la Empresa</h4>
                                     <p>Bachelor of Business Administration (BBA)</p>
                                     <p>International Business</p>
                                     <p>2018</p>
                                     <p className="italic">I had a great opportunity to spent whole spring semester doing business studies with global and Latin American focus in Buenos Aires, Argentina.</p>
                                 </li>
-                                <li>
+                                <li data-aos="fade-up" data-aos-duration="2000" data-aos-offset="100">
                                     <h4>Varkauden lukio</h4>
                                     <p>Secondary School Graduate</p>
                                     <p>2011–2014</p>
@@ -49,12 +56,12 @@ const History = () => {
                                 <h2>Work</h2>
                             </div>
                             <ul className="history-timeline">
-                                <li>
+                                <li data-aos="fade-up" data-aos-duration="2000" data-aos-offset="100">
                                     <h4>Aava &amp; Bang Oy</h4>
                                     <p>Web Developer</p>
                                     <p>8/2018–</p>
                                 </li>
-                                <li>
+                                <li data-aos="fade-up" data-aos-duration="2000" data-aos-offset="100">
                                     <h4>Freelancer</h4>
                                     <p>Web Developer &amp; Designer</p>
                                     <p>1/2016–8/2018</p>              
